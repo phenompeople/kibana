7f9954e30bd9d88ae76d41697d1497e6ea6ba7ac
Rajesh Jonnalagadda <rajesh.jonnalagadda@phenompeople.com>
2018-06-25 13:03:57 +0530

    Adding Kibana 5.6.9

---
+++ 5.6.9/setup/scripts/kibana.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -o nounset # Treat unset variables as an error
+ELASTICSEARCH_URL=${ELASTICSEARCH_URL:-http://localhost:9200}
+KIBANA_INDEX=${KIBANA_INDEX:-.kibana-6}
+SERVER_HOST=${SERVER_HOST:-localhost}
+ES_REQUEST_TIMEOUT=${ES_REQUEST_TIMEOUT:-60000}
+sed -ri "s|^(\\#\\s*)?(elasticsearch.url:).*|\\2 '$ELASTICSEARCH_URL'|; \
+                s|^(\\#\\s*)?(server.host:).*|\\2 '$SERVER_HOST'|; \
+                s|^(\\#\\s*)?(elasticsearch.requestTimeout:).*|\\2 '$ES_REQUEST_TIMEOUT'|; \
+                s|^(\\#\\s*)?(kibana.index:).*|\\2 '$KIBANA_INDEX'|; " /etc/kibana/kibana.yml
+exec su -l kibana -s /bin/bash -c "/usr/local/kibana/bin/kibana -c /etc/kibana/kibana.yml" > /var/log/kibana/kibana.log

c481be5943e844ad39036f4381a1e2996e0a9700
harshavardhan <harshavardhan.baddam@phenompeople.com>
2020-04-23 17:31:24 +0530

    updating the config elasticsearch.hosts in kibana 7.6.0 version

--- 7.6.0/setup/scripts/kibana.sh
+++ 7.6.0/setup/scripts/kibana.sh
@@ -4,7 +4,7 @@ ELASTICSEARCH_URL=${ELASTICSEARCH_URL:-http://localhost:9200}
 KIBANA_INDEX=${KIBANA_INDEX:-.kibana-6}
 SERVER_HOST=${SERVER_HOST:-localhost}
 ES_REQUEST_TIMEOUT=${ES_REQUEST_TIMEOUT:-60000}
-sed -ri "s|^(\\#\\s*)?(elasticsearch.url:).*|\\2 '$ELASTICSEARCH_URL'|; \
+sed -ri "s|^(\\#\\s*)?(elasticsearch.hosts:).*|\\2 '$ELASTICSEARCH_URL'|; \
                 s|^(\\#\\s*)?(server.host:).*|\\2 '$SERVER_HOST'|; \
                 s|^(\\#\\s*)?(elasticsearch.requestTimeout:).*|\\2 '$ES_REQUEST_TIMEOUT'|; \
                 s|^(\\#\\s*)?(kibana.index:).*|\\2 '$KIBANA_INDEX'|; " /etc/kibana/kibana.yml
